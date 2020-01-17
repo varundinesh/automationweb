42b94e7b9a5246dc6797bb35e905f93e42fac665
varun dineshan <varun4dinesh@gmail.com>
2020-01-17 12:11:07 -0500

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,11 +1,13 @@
+#python code to send messages to the phone using twilio API
 from twilio.rest import Client
 # Your Account SID from twilio.com/console
+#expired tokens
 account_sid = "ACb85230b120e3acadebe893cf0bd56e6c"
 # Your Auth Token from twilio.com/console
 auth_token  = "628041d6ba8ced55fbf5c350b1f3888a"
 x=40
 client = Client(account_sid, auth_token)
-
+#creating the message structure.
 message = client.messages.create(
     to="+91 9496308948",
     from_="+1 774-567-7189 ",
